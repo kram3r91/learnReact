45d92d07cc99a5b1854049a9c863466f0c88f7fc
Unknown <claudiu@megheaiulian.com>
2019-01-28 01:51:06 +0200

    Props lesson

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,14 @@
 import React, { Component } from 'react';
+import Ninjas from './Ninjas.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>My first React app!</h1>
+        <p>Welcome :)</p>
+        <Ninjas name="Ryu" age="25" belt="black" />
+        <Ninjas name="Claudiu" age="27" belt="blue" />
       </div>
     );
   }
